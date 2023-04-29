c66672a6a99d6a865faa992ec0bc38348d399dff
james-minor <james.minor.business@gmail.com>
2023-04-28 23:16:54 -0500

    Implemented behavior for /game add

--- src/cogs/Game.py
+++ src/cogs/Game.py
@@ -16,8 +16,28 @@ class Game(commands.Cog):
     @game.command(name='add', description="Adds a custom game to the games database.")
     @commands.has_permissions(administrator=True)
     async def add_game_command(self, ctx, game_title: str):
-        await ctx.respond(f'Adding custom game **{game_title}**.')
-        # TODO: implement behavior
+        # Seeing if an entry for this game already exists.
+        cursor = self.connection.cursor()
+        cursor.execute(
+            """
+            SELECT COUNT(*) FROM tb_custom_games
+            WHERE game_title = ?;
+            """,
+            [game_title]
+        )
+
+        # Only adding the entry if it does not already exist.
+        if cursor.fetchone()[0] == 0:
+            cursor.execute(
+                """
+                INSERT INTO  tb_custom_games(game_title)
+                VALUES(?);
+                """,
+                [game_title]
+            )
+            await ctx.respond(f'Added custom game **{game_title}**.')
+        else:
+            await ctx.respond(f'Custom game **{game_title}** already exists!')
 
     @game.command(name='remove', description="Removes a custom game from the games database.")
     @commands.has_permissions(administrator=True)
