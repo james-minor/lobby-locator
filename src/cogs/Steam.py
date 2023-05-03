31c2c443beeb0d15e3a4304c5b6c7479a220bef5
james-minor <james.minor.business@gmail.com>
2023-05-03 12:27:09 -0500

    Added documentation for Steam.py

--- src/cogs/Steam.py
+++ src/cogs/Steam.py
@@ -8,16 +8,37 @@ from logging import Logger
 
 
 class Steam(commands.Cog):
+    """
+    The Steam class contains definitions for any commands using the 'steam' prefix.
+    """
 
     steam = discord.SlashCommandGroup('steam', 'Editing saved Steam IDs')
 
     def __init__(self, bot: Bot, sql_connection: Connection, logger: Logger):
+        """
+        Constructor for the Steam class.
+
+        :param bot: The Discord bot object.
+        :param sql_connection: The current database connection.
+        :param logger: The logging object.
+        """
+
         self.bot = bot
         self.connection = sql_connection
         self.logger = logger
 
     @steam.command(name='set', description='Sets your saved Steam ID')
-    async def set_command(self, ctx, steam_id: str):
+    async def set_command(self, ctx, steam_id: str) -> None:
+        """
+        Sets the current user's saved Steam ID. **Note:** Users must use their Steam64 ID when setting their ID.
+
+        Example: **/steam set 123456789**
+
+        :param ctx: The command context
+        :param steam_id: The Steam64 ID of the current user.
+        :return: None
+        """
+
         await ctx.respond(f'Setting Steam ID: **{steam_id}**, for user: **{ctx.author}**', ephemeral=True)
 
         # Seeing if an entry for this user already exists, if so UPDATE instead of INSERTING data.
@@ -53,7 +74,16 @@ class Steam(commands.Cog):
         self.connection.commit()
 
     @steam.command(name='remove', description='Removes your saved Steam ID')
-    async def remove_command(self, ctx):
+    async def remove_command(self, ctx) -> None:
+        """
+        Removes the current user's Steam ID from the database.
+
+        Example: **/steam remove**
+
+        :param ctx: The command context.
+        :return: None
+        """
+
         cursor = self.connection.cursor()
 
         # Removing the user entry from tb_steam_users.
