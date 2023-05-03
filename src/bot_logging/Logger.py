aee3fbc16cce91e7255cb7cf2a62e8c93121d00c
james-minor <james.minor.business@gmail.com>
2023-05-03 12:10:14 -0500

    Added documentation to Logger.py

--- src/bot_logging/Logger.py
+++ src/bot_logging/Logger.py
@@ -5,21 +5,66 @@ from datetime import datetime
 
 
 class Logger:
+    """
+    Custom logger for the Discord bot.
+    """
+
+    _log_directory: str
+    """
+    The directory where log files should be stored.
+    """
+
+    _log_to_console: bool
+    """
+    If True, the Logger will log any information it receives to the console as well as writing to the log file.
+    """
 
     def __init__(self, log_directory: str, log_to_console: bool = True):
+        """
+        Constructor for the Logger class.
+
+        :param log_directory:
+        :param log_to_console:
+        """
+
         self._log_directory = log_directory
         self.log_to_console = log_to_console
 
     def info(self, message: str) -> None:
+        """
+        Logs a message with the INFO tag to the log file.
+
+        :param message: The info / debug message to log.
+        :return: None
+        """
         self.log(LogLevel.INFO, message)
 
     def warn(self, message: str) -> None:
+        """
+        Logs a message with the WARN tag to the log file.
+
+        :param message: The warning message to log.
+        :return: None
+        """
         self.log(LogLevel.WARN, message)
 
     def critical(self, message: str) -> None:
+        """
+        Logs a message with the CRITICAL tag to the log file.
+
+        :param message: The error message to log.
+        :return: None
+        """
         self.log(LogLevel.CRITICAL, message)
 
     def log(self, log_level: LogLevel, message: str) -> None:
+        """
+        The internal logging method that is used to log messages to the current logging file.
+
+        :param log_level: The LogLevel of the message to log.
+        :param message: The message to log.
+        :return: None
+        """
         with open(os.path.join(self._log_directory, datetime.today().strftime('%m-%d-%Y') + '.log'), 'a') as file:
             date_time_tag = f'[{datetime.today().strftime("%m-%d-%Y %H:%M:%S")}]'
             file.write(date_time_tag + f'[{log_level.value[0]}]: {message}\n')
