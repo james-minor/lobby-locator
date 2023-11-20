f4dc30737c3d22b893babfe15b7f97ec671279bc
James Minor <james.minor.business@gmail.com>
2023-11-20 11:38:03 -0600

    Steam class now stores apps in an in-memory database.

--- src/main/java/com/james_minor/gameseeker/Steam.java
+++ src/main/java/com/james_minor/gameseeker/Steam.java
@@ -6,22 +6,49 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Steam
 {
-	private ArrayList<SteamApp> apps = new ArrayList<SteamApp>();
+	private static final String steamDatabaseURL = "jdbc:sqlite::memory:";
 
-	public Steam(String apiUrl)
+	private static final String steamAppURL = "https://api.steampowered.com/ISteamApps/GetAppList/v0002";
+
+	private final Connection connection;
+
+	public Steam()
 	{
+		/* Initializing the Steam app database tables.
+		 */
+		try
+		{
+			this.connection = DriverManager.getConnection(steamDatabaseURL);
+			this.connection.prepareStatement(
+					"CREATE TABLE IF NOT EXISTS steam_apps (id INT PRIMARY KEY, name VARCHAR(255))"
+			).executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			throw new RuntimeException(e);
+		}
+
+		/* Initializing the HTTP client for the Steam API.
+		 */
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(apiUrl))
+				.uri(URI.create(steamAppURL))
 				.GET()
 				.build();
 
+		/* Attempting to fetch all listed Steam applications from the Steam API.
+		 */
 		try
 		{
+			long startTime = System.nanoTime();
+
 			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 			if (response.statusCode() != 200)
 			{
@@ -34,11 +61,16 @@ public class Steam
 			for (int i = 0; i < jsonArray.length(); i++)
 			{
 				JSONObject app = jsonArray.getJSONObject(i);
-				System.out.println(app);
-
-				// TODO: create an SQLite database.
-				// TODO: store apps within an SQLite Database.
+				if (app.getString("name").length() > 0)
+				{
+					addSteamApp(app.getInt("appid"), app.getString("name"));
+				}
 			}
+
+			long endTime = System.nanoTime();
+			long executionTime = (endTime - startTime) / 1_000_000;
+
+			System.out.printf("Added %d Steam Apps in %d milliseconds.%n", jsonArray.length(), executionTime);
 		}
 		catch(Exception e)
 		{
@@ -46,8 +78,17 @@ public class Steam
 		}
 	}
 
-	public ArrayList<SteamApp> getApps()
+	private void addSteamApp(int id, String name)
 	{
-		return this.apps;
+		try (PreparedStatement statement = this.connection.prepareStatement(
+				"INSERT OR REPLACE INTO steam_apps (id, name) VALUES (?, ?)"
+		)) {
+			statement.setInt(1, id);
+			statement.setString(2, name);
+			statement.executeUpdate();
+		} catch (SQLException e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
 }
