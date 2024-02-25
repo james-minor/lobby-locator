9d3b88e3d2ca7381080f8a79960e07c6c10057e1
James Minor <james.minor.business@gmail.com>
2024-02-24 18:47:56 -0600

    Added database package

---
+++ src/database/database.py
@@ -0,0 +1,101 @@
+import sqlite3
+from sqlite3 import Error, Connection
+
+
+class Database:
+    """Class to handle communicating with the SQL database."""
+
+    connection: None | Connection
+    """The connection to the SQL database."""
+
+    def __init__(self):
+        self.connection = None
+
+    def connect(self, connection_string: str) -> bool:
+        """
+        Connects to an SQL database via the passed connection_string parameter, returns True if the connection is
+        successful, False otherwise.
+
+        :param connection_string: The connection string for the database.
+        """
+
+        try:
+            self.connection = sqlite3.connect(connection_string)
+            return True
+        except Error as error:
+            print(error)
+
+        return False
+
+    def disconnect(self) -> None:
+        """Safely closes the SQLite database connection."""
+
+        if self.connection is not None:
+            self.connection.commit()
+            self.connection.close()
+            self.connection = None
+
+    def create_tables(self) -> bool:
+        """
+        Creates the tables in the database.
+
+        :return: True if the tables were created successfully, False otherwise.
+        """
+
+        if self.connection is None:
+            return False
+
+        cursor = self.connection.cursor()
+        cursor.executescript(
+            """
+            CREATE TABLE IF NOT EXISTS tb_users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                discord_id VARCHAR(18) NOT NULL,
+                steam_id VARCHAR(17)
+            );
+
+            CREATE TABLE IF NOT EXISTS tb_steam_games(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                steam_id INTEGER NOT NULL,
+                game_title VARCHAR(100) NOT NULL,
+                game_title_lower VARCHAR(100) NOT NULL,
+                UNIQUE(steam_id)
+            );
+            """
+        )
+
+        return True
+
+    def refresh_steam_games(self, steam_apps: dict[int, str]) -> int:
+        """
+        Deletes and then repopulates the steam_games database table.
+
+        :param steam_apps: Dictionary of steam_apps to repopulate with. If passed an empty dictionary, the table
+        will not be emptied, and the current state of the table will be maintained.
+
+        :return: The number of steam games added to the database.
+        """
+
+        # Safeguard against empty dictionaries clearing out the games table.
+        if len(steam_apps) == 0:
+            return 0
+
+        # Truncating the steam games table.
+        cursor = self.connection.cursor()
+        cursor.execute("DELETE FROM tb_steam_games")
+
+        # Inserting new data into the steam games table.
+        for app_id in steam_apps:
+            cursor.execute(
+                """
+                INSERT INTO tb_steam_games(steam_id, game_title, game_title_lower) 
+                VALUES (?, ?, ?)
+                """,
+                [
+                    app_id,
+                    steam_apps[app_id],
+                    steam_apps[app_id].lower(),
+                ]
+            )
+
+        return cursor.lastrowid
---
+++ src/database/test_database.py
@@ -0,0 +1,64 @@
+import os
+import unittest
+
+from database import Database
+
+
+class ConnectionTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.database = Database()
+
+    def test_opening_connection(self):
+        self.assertTrue(self.database.connect('test.sqlite'))
+
+    def test_closing_connection(self):
+        self.database.connect('test.sqlite')
+        self.database.disconnect()
+        self.assertIsNone(self.database.connection)
+
+    def tearDown(self):
+        self.database.disconnect()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove('test.sqlite')
+
+
+class CreateTablesTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.database = Database()
+
+    def test_bad_connection(self):
+        self.assertFalse(self.database.create_tables())
+
+    def test_all_tables_created(self):
+        self.database.connect('test.sqlite')
+        self.assertTrue(self.database.create_tables())
+
+    def tearDown(self):
+        self.database.disconnect()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove('test.sqlite')
+
+
+class RefreshingSteamGamesTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.database = Database()
+        self.database.connect('test.sqlite')
+        self.database.create_tables()
+
+    def test_empty_games_dictionary(self):
+        self.database.refresh_steam_games({0: 'test 1', 1: 'test 2', 2: 'test 3'})
+        self.assertEqual(self.database.refresh_steam_games({}), 0)
+
+    def test_all_games_refreshed(self):
+        self.assertEqual(self.database.refresh_steam_games({0: 'test 1', 1: 'test 2', 2: 'test 3'}), 3)
+
+    def tearDown(self):
+        self.database.disconnect()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove('test.sqlite')
