f60443c3a02b08a8a05872e54c4a404bb40965d6
James Minor <james.minor.business@gmail.com>
2024-03-01 23:43:51 -0600

    Added fk column steam_user_id to tb_owned_games

--- src/database/database_wrapper.py
+++ src/database/database_wrapper.py
@@ -71,7 +71,9 @@ class DatabaseWrapper:
                 -- Table to hold data for games owned by registered users.
                 CREATE TABLE IF NOT EXISTS tb_owned_games(
                     steam_app_id INTEGER PRIMARY KEY,
-                    FOREIGN KEY (steam_app_id) REFERENCES tb_steam_apps(steam_app_id)
+                    steam_user_id VARCHAR(17) NOT NULL,
+                    FOREIGN KEY (steam_app_id) REFERENCES tb_steam_apps(steam_app_id),
+                    FOREIGN KEY (steam_user_id) REFERENCES tb_users(steam_user_id)
                 );
                 """
             )
