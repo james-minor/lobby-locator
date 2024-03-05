10c3026331fb4ba7a64bdefa523d9e7296487d5d
James Minor <james.minor.business@gmail.com>
2024-03-05 13:22:24 -0600

    Added unit tests for the is_open method

--- src/database/__tests__/test_connection.py
+++ src/database/__tests__/test_connection.py
@@ -17,3 +17,19 @@ class DisconnectMethodTests(unittest.TestCase):
     def test_disconnect_on_closed_connection(self):
         self.connection.close()
         self.assertFalse(self.connection.disconnect())
+
+
+class IsOpenMethodTests(unittest.TestCase):
+    """
+    Test cases for the Connection.is_open() method.
+    """
+
+    def setUp(self) -> None:
+        self.connection: Connection = Connection(':memory:')
+
+    def test_after_initialization(self) -> None:
+        self.assertTrue(self.connection.is_open())
+
+    def test_after_disconnect(self) -> None:
+        self.connection.disconnect()
+        self.assertFalse(self.connection.is_open())
